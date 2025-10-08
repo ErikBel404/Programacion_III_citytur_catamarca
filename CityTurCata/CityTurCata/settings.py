619c4c2df990e53e59d36b19318e7856f5ade156
Lala <alejomaidana29@gmail.com>
2025-10-08 01:04:25 -0300

    configuraciones del back

--- CityTurCata/CityTurCata/settings.py
+++ CityTurCata/CityTurCata/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ii!txaz-=p33bm61py9r41k3h%!@%9h_p5_1wi@4rnuevr7d2^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -76,6 +77,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASES = {
+    "default": {
+        "ENGINE": config("POSTGRES_ENGINE", default ="django.db.backends.postgresql"),
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
---
+++ CityTurCata/apps/seguridadControl/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SeguridadcontrolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'seguridadControl'
