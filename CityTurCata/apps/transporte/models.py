c998edd7164fceecc0aeb6e2128c24b501439280
Lala <alejomaidana29@gmail.com>
2025-10-15 15:53:25 -0300

    modelos de reporte reserva transporte

--- CityTurCata/apps/transporte/models.py
+++ CityTurCata/apps/transporte/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Transporte(models.Model):
+    dominioMatriculaTransporte = models.TextField(max_length=250, unique=True);
+    capacidadTransporte = models.IntegerField(blank=False, null=False);
+    estadoTransporte = models.TextField(max_length=250, blank= False, null= False);
+
+    def __str__(self):
+        return f'Matricula del Transporte: {self.dominioMatriculaTransporte}, Capacidad del transporte: {self.capacidadTransporte}, estado del Transporte: {self.estadoTransporte}'
+    
