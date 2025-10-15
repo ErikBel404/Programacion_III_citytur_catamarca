a616ee34788e1bf191663d0f0039f2ecd0b6566c
E.M.Cirione.L <erikcirione@gmail.com>
2025-10-15 18:40:22 -0300

    unga unga

--- CityTurCata/apps/perfil/models.py
+++ CityTurCata/apps/perfil/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Persona(models.Model):
-    dni = models.CharField(max_length=8, unique=True)
+    dni = models.TextField(max_length=8, unique=True)
     correo = models.TextField(max_length=100, blank= False, null= False )
     apellido= models.TextField(max_length=50, blank= False, null= False)
     nombre = models.TextField(max_length=20, blank= False, null= False )
     usuario= models.TextField(max_length=15, blank= False, null= False )
-    contraseña= models.TextField(min_length=8, blank= False, null= False )
+    contraseña = models.CharField( max_length=100, validators=[MinLengthValidator(8)], blank=False, null= False)
 
 
     def __str__(self):
