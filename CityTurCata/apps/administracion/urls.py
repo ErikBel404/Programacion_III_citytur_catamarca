3d1defffe88ee6e5b615a66f5a3c29579d88f001
Lala <alejomaidana29@gmail.com>
2025-10-17 19:58:04 -0300

    urls y view de de transporte y reportes

--- CityTurCata/apps/administracion/urls.py
+++ CityTurCata/apps/administracion/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from apps.administracion.views import lista_transportes_view,detalle_transporte_view
+from apps.administracion.views import listaTransportesView,detalleTransporteView,registraTransporteView,modificarTransporteView
+from apps.administracion.views import reportesView,reporteRecorridosActivosView,reporteParadasMasUtilizadasView,reporteReservasRecorridoView,reporteConsultaReservas,reporteEstadistaPasajeros
 
 app_name = 'administracion'
 
 urlpatterns = [
-    path('transportes/',lista_transportes_view, name='listaTransporte'),
-    path('transportes/<int:id>/',detalle_transporte_view, name='detalleTransporte')
+    #urls de transporte
+    path('transportes/',listaTransportesView, name='listaTransporte'),
+    path('transportes/<int:id>/',detalleTransporteView, name='detalleTransporte'),
+    path('transportes/agregar/',registraTransporteView, name='registoTransporte'),
+    path('transporte/modificar/', modificarTransporteView, name='modificarTransporte'),
+    #urls de reportes
+    path('reportes/', reportesView, name='reportesTodos'),
+    path('reportes/activos/',reporteRecorridosActivosView, name='recorridosActivos'),
+    path('reportes/paradas/',reporteParadasMasUtilizadasView,name ='paradasMasUtilizadas'),
+    path('reportes/reservas/',reporteReservasRecorridoView, name= 'reservaRecorrido'),
+    path('reportes/consultas/', reporteConsultaReservas, name='consultasReservas'),
+    path('reportes/estadisticas/', reporteEstadistaPasajeros, name='estadisticasPasajeros')
+
 ]
--- CityTurCata/apps/administracion/views.py
+++ CityTurCata/apps/administracion/views.py
@@ -2,20 +2,40 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from apps.administracion.models import Transporte
+from apps.administracion.models import Reportes
 
 # Create your views here.
 
 #Definicion de transporte
-def lista_transportes_view(request):
+def listaTransportesView(request):
     #return render(request, 'administracion/transporte.html')   -> suponiendo que creamos una carpetea template dentro de la misma ponemos una carpeta administracion y dentro de ella tenemos el html de transporte
+    return HttpResponse('Aca sale la lista de transportes');
 
-    return HttpResponse('Aca sale la lista de transportes')
+def detalleTransporteView(request, id):
+    return HttpResponse(f'Aca sale un transporte en detalle con id:{id}');
 
+def registraTransporteView(request):
+    return HttpResponse('Aca se registran los transportes solamente');
 
-def detalle_transporte_view(request, id):
-    
-    return HttpResponse(f'Aca sale un transporte en detalle con id:{id}')
+def modificarTransporteView(request):
+    return HttpResponse('Aca se modifica el transporte');
 
+#Definicion de reportes
 
+def reportesView(request):
+    return HttpResponse('Aca es la pagina de los resportes');
 
-#Definicion
+def reporteRecorridosActivosView(request):
+    return HttpResponse('Aca va el reporte de recorridos activos');
+
+def reporteParadasMasUtilizadasView(request):
+    return HttpResponse('Aca van el reporte de las paradas mas utilizadas');
+
+def reporteReservasRecorridoView(resquest):
+    return HttpResponse('Aca va el reporte de las reservas de los recorridos');
+
+def reporteConsultaReservas(request):
+    return HttpResponse('Aca va el reporte de las consultas de las reservas');
+
+def reporteEstadistaPasajeros(request):
+    return HttpResponse('Aca va el reporte de estadisticas en un rango de fechas')
