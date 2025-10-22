4563989b5c0a41118eaf5fdf34641f42c2b6a64c
E.M.Cirione.L <erikcirione@gmail.com>
2025-10-22 10:39:10 -0300

    cambio modelo reserva

--- CityTurCata/apps/reservas/models.py
+++ CityTurCata/apps/reservas/models.py
@@ -4,12 +4,12 @@ from apps.administracion.models  import Itinerario
 
 # Create your models here.
 class Reserva(models.Model):
-    recorridoReserva = models.TextField(max_length=250, blank=False, null=False);
+    recorridoReserva = models.CharField(max_length=250, blank=False, null=False);
     cantidadReserva = models.IntegerField(blank=False, null=False);
     fechaReserva = models.DateField(blank=False, null=False);
     horaReserva = models.TimeField(blank=False, null=False);
-    puntoDePartidaReserva = models.TextField(blank=False, null=False);
-    estadoReseva = models.TextField(max_length=250, blank=False, null=False);
+    puntoDePartidaReserva = models.CharField(blank=False, null=False);
+    estadoReseva = models.CharField(max_length=250, blank=False, null=False);
 
     turista= models.ForeignKey (Turista, on_delete= models.CASCADE, related_name='reservaTurista');
     itinerario= models.ForeignKey (Itinerario, on_delete=models.CASCADE, related_name='reservaitinerario')
