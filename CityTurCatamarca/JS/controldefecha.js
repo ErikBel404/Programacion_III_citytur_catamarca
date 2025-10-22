080718fee17ff98c34cb4cead22c88edec96ae11
E.M.Cirione.L <erikcirione@gmail.com>
2025-10-22 03:02:10 -0300

    ya funciono el css

--- CityTurCatamarca/JS/controldefecha.js
+++ CityTurCatamarca/JS/controldefecha.js
@@ -3,6 +3,7 @@
 document.addEventListener ("DOMContentLoaded", ()=> {
 
     const campoFechaUsuario = document.getElementById ("fecha-reservaPc");
+    const labelFechaUsuario = document.getElementById ("labelFechaPc");
 
     campoFechaUsuario.addEventListener ("change", ()=>{
         const fechaUsuario = new Date (campoFechaUsuario.value);
@@ -11,6 +12,8 @@ document.addEventListener ("DOMContentLoaded", ()=> {
         if (fechaUsuario < fechaActual){
             campoFechaUsuario.style.color = "red";
             campoFechaUsuario.style.border = "5px solid red";
+            labelFechaUsuario.style.top = "1px";
+            labelFechaUsuario.style.fontSize = "12px"
             campoFechaUsuario.setCustomValidity("Fecha invalida");
             
             
